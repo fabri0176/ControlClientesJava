0f5a5cf412f9f6fcda286b219ee12fe7cc3a27a4
Nestor F Parra <nparra@existaya.com>
2020-08-09 17:09:17 -0500

    configuración clase conexion

---
+++ src/main/java/datos/Conexion.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+
+
+
+/**
+ *
+ * @author fabricio
+ */
+public class Conexion {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_clientes_java?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "";
+
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(50);//50 Conexiones de forma inicial
+        return ds;
+    }
+
+    public static Connection getConnection() throws SQLException{
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(Connection conm){
+        try {
+            conm.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
