8bbe9271c98cfb6f7fcd2a0dec7b34d7af9756b4
Nestor F Parra <nparra@existaya.com>
2020-08-15 16:57:06 -0500

    Crear Cliente

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -11,32 +11,35 @@ import java.util.logging.Logger;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
-
-
-
 /**
  *
  * @author fabricio
  */
 public class Conexion {
+
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_clientes_java?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "";
 
-    public static DataSource getDataSource(){
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(JDBC_URL);
-        ds.setUsername(JDBC_USER);
-        ds.setPassword(JDBC_PASSWORD);
-        ds.setInitialSize(50);//50 Conexiones de forma inicial
-        return ds;
+    private static BasicDataSource dataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(JDBC_PASSWORD);
+            dataSource.setInitialSize(50);//50 Conexiones de forma inicial
+        }
+
+        return dataSource;
     }
 
-    public static Connection getConnection() throws SQLException{
+    public static Connection getConnection() throws SQLException {
         return getDataSource().getConnection();
     }
 
-    public static void close(ResultSet rs){
+    public static void close(ResultSet rs) {
         try {
             rs.close();
         } catch (SQLException ex) {
@@ -44,7 +47,7 @@ public class Conexion {
         }
     }
 
-    public static void close(PreparedStatement stmt){
+    public static void close(PreparedStatement stmt) {
         try {
             stmt.close();
         } catch (SQLException ex) {
@@ -52,7 +55,7 @@ public class Conexion {
         }
     }
 
-    public static void close(Connection conm){
+    public static void close(Connection conm) {
         try {
             conm.close();
         } catch (SQLException ex) {
